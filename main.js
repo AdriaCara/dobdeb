4f5e0404cbf39aa4985fffe9a5f533c8ca0732d0
AdriaCara <adriaProgrammer@gmail.com>
2022-04-01 04:19:46 +0200

    Node and new JS code.

---
+++ main.js
@@ -0,0 +1,8 @@
+const loginForm = document.querySelector('#loginForm');
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const loginEmail = document.querySelector('#loginEmail').value;
+    const loginPassword = document.querySelector('#loginPassword').value;
+});
