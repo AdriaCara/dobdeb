fc69ac3d6e600af40c16d4f4a6e56a3e91ccc338
AdriaCara <adriaProgrammer@gmail.com>
2022-04-28 17:18:29 +0200

    Changed JavaScript to PHP

--- main.js
+++
@@ -1,8 +0,0 @@
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ],
-    // Other config options...
-  });
